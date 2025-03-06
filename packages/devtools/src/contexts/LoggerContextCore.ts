49ebcf9d3496b073a2fe82cff356d21494ff4cc0
Corina Gum <>
2025-03-06 15:08:07 -0800

    Remove LoggerContext

--- packages/devtools/src/contexts/LoggerContextCore.ts
+++
@@ -1,5 +0,0 @@
-import { ConsoleLogger } from '@microsoft/spark.common/logging';
-import { createContext } from 'react';
-
-export const logger = new ConsoleLogger('@spark/devtools');
-export const LoggerContext = createContext<ConsoleLogger>(logger);
