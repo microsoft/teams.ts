b05e2c126adacb0d4a6b7e30ac342bb84eaf3ca9
Corina Gum <>
2025-03-21 21:59:23 -0700

    Keep error styling for the future

--- packages/devtools/src/components/ComposeBox/ComposeBox.styles.ts
+++ packages/devtools/src/components/ComposeBox/ComposeBox.styles.ts
@@ -1,4 +1,4 @@
-import { makeStyles } from '@fluentui/react-components';
+import { makeStyles, tokens } from '@fluentui/react-components';
 
 const useComposeBoxClasses = makeStyles({
   composeBoxContainer: {
@@ -7,6 +7,18 @@ const useComposeBoxClasses = makeStyles({
   },
   composeInput: {
     width: '100%',
+  },
+  error: {
+    border: `${tokens.strokeWidthThin} solid ${tokens.colorPaletteRedBorder2}`,
+    '&:focus-within': {
+      border: `${tokens.strokeWidthThin} solid ${tokens.colorPaletteRedBorder2}`,
+    },
+  },
+  errorMessage: {
+    color: tokens.colorPaletteRedForeground1,
+    fontSize: tokens.fontSizeBase200,
+    marginTop: tokens.spacingVerticalXXS,
+    marginBottom: tokens.spacingVerticalXXS,
   },
 });
 
