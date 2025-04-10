74e8476992bc70fd046529e4476de9f77e3f9a05
heyitsaamir <ajawaid191@gmail.com>
2025-04-10 14:08:48 -0700

    Remove usage

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -8,22 +8,7 @@ import { IContext } from './context';
 import { EnvStorage } from './environment';
 import { Settings } from './settings';
 
-// Check Node.js version
-const checkNodeVersion = () => {
-  const currentVersion = process.versions.node;
-  const majorVersion = parseInt(currentVersion.split('.')[0], 10);
-
-  if (majorVersion < 20) {
-    console.error(
-      `Error: Node.js version 20 or higher is required. Current version: ${currentVersion}`
-    );
-    process.exit(1);
-  }
-};
-
 (async () => {
-  checkNodeVersion();
-
   const settings = Settings.load();
   const envs = EnvStorage.load(settings);
   const context: IContext = { settings, envs };
