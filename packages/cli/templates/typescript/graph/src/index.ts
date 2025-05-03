674956d46b54e00a3ffa42810a4fc690a772c551
heyitsaamir <ajawaid191@gmail.com>
2025-05-03 13:05:05 -0700

    Test

--- packages/cli/templates/typescript/graph/src/index.ts
+++ packages/cli/templates/typescript/graph/src/index.ts
@@ -20,7 +20,7 @@ app.event('signin', async ({ send, userGraph }) => {
   const me = await userGraph.me.get();
 
   await send(
-    new MessageActivity(`hello ${me.displayName} 👋!`).addCard(
+    new MessageActivity(`Hello ${me.displayName} 👋!`).addCard(
       'adaptive',
       new Card(
         new CodeBlock({
