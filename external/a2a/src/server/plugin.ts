30dfaa798a3dcc0ca4897821dfe4e234bef29fcb
heyitsaamir <ajawaid191@gmail.com>
2025-05-10 16:39:06 -0700

    fx lint

--- external/a2a/src/server/plugin.ts
+++ external/a2a/src/server/plugin.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { Dependency, EmitPluginEvent, Event, HttpPlugin, IPlugin, Logger, Plugin, Storage } from '@microsoft/teams.apps';
 import { ILogger, IStorage } from '@microsoft/teams.common';
 
+import { A2AAgentManager } from '../client/a2a-agent-manager';
 import * as schema from '../common/schema';
 
 import { isTaskRequest } from './middleware/isTaskRequest';
