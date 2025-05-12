758709c74551d28652926c3ed7214b93709bb9bc
Kavin Singh <kavinsingh@hotmail.com>
2025-05-12 22:59:30 +0700

    pr feedback

--- tests/auth/src/index.ts
+++ tests/auth/src/index.ts
@@ -1,16 +1,16 @@
 import { App } from '@microsoft/teams.apps';
 import { ConsoleLogger } from '@microsoft/teams.common/logging';
-import { DevtoolsPlugin } from '@microsoft/teams.dev';
 
 // :snippet-start: auth-config
 const app = new App({ 
   oauth: { // oauth configurations
-    // the name of the auth connection to use.
-    // it should be the same as the OAuth connection name defined in the Azure Bot configuration.
+    /**
+     * The name of the auth connection to use.
+     * It should be the same as the OAuth connection name defined in the Azure Bot configuration.
+     */
     defaultConnectionName: 'graph' 
   },
-  logger: new ConsoleLogger('@tests/auth', { level: 'debug' }), // optional
-  plugins: [new DevtoolsPlugin()], // optional
+  logger: new ConsoleLogger('@tests/auth', { level: 'debug' })
 });
 // :snippet-end: auth-config
 
