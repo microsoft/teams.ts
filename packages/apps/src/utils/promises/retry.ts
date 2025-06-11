e15a47df54492703a067eb6752a40f6095dbef5b
Alex Acebo <aacebowork@gmail.com>
2025-06-11 14:56:25 -0700

    fix streaming retry logic

--- packages/apps/src/utils/promises/retry.ts
+++ packages/apps/src/utils/promises/retry.ts
@@ -1,3 +1,5 @@
+import { ILogger } from '@microsoft/teams.common';
+
 export type RetryOptions = {
   /**
    * the max number of retry attempts
@@ -7,23 +9,35 @@ export type RetryOptions = {
 
   /**
    * the delay in ms per retry
-   * @default 200
    */
   readonly delay?: number;
+
+  /**
+   * the logger to use
+   */
+  readonly logger?: ILogger;
 };
 
-export async function retry<T = any>(promise: Promise<T>, options?: RetryOptions) {
-  const max = options?.max || 3;
-  const delay = options?.delay || 200;
+export async function retry<T = any>(promise: () => Promise<T>, options?: RetryOptions) {
+  const max = options?.max ?? 5;
+  const delay = options?.delay ?? 500;
+  const log = options?.logger?.child('retry');
 
   try {
-    return await promise;
+    return await promise();
   } catch (err) {
     if (max > 0) {
+      log?.debug(`delaying ${delay}ms...`);
       await new Promise((resolve) => setTimeout(resolve, delay));
-      return retry(promise, { max: max - 1, delay: delay * 2 });
+      log?.debug('retrying...');
+      return retry(promise, {
+        max: max - 1,
+        delay: delay * 2,
+        logger: options?.logger,
+      });
     }
 
+    log?.error(err);
     throw err;
   }
 }
