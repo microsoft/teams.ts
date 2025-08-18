1d796709f5b069cbf23bff23695f6f183cfb6d63
jesperholmbergmsft <202218569+Jesperholmbergmsft@users.noreply.github.com>
2025-08-18 11:28:20 -0700

    Fix graph-tools build

---
+++ packages/graph-tools/tsup.config.js
@@ -0,0 +1,12 @@
+const base = require('@microsoft/teams.config/tsup.config');
+
+/**
+ * @type {import('tsup').Options}
+ */
+module.exports = {
+  ...base,
+  minify: true,
+  bundle: true,
+  entry: ['src/index.ts'],
+  publicDir: 'templates',
+};
