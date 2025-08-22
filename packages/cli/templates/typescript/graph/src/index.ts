c1839734fd2e7d71df6979595da52f351f4c00c9
jesperholmbergmsft <202218569+Jesperholmbergmsft@users.noreply.github.com>
2025-08-22 15:42:02 -0700

    PR feedback: Update ATK templates too

--- packages/cli/templates/typescript/graph/src/index.ts
+++ packages/cli/templates/typescript/graph/src/index.ts
@@ -2,26 +2,27 @@ import { MessageActivity } from '@microsoft/teams.api';
 import { App } from '@microsoft/teams.apps';
 import { AdaptiveCard, CodeBlock } from '@microsoft/teams.cards';
 import { DevtoolsPlugin } from '@microsoft/teams.dev';
+import * as endpoints from '@microsoft/teams.graph-endpoints';
 
 const app = new App({
   plugins: [new DevtoolsPlugin()],
 });
 
-app.on("message", async ({ log, signin, isSignedIn }) => {
+app.on('message', async ({ log, signin, isSignedIn }) => {
   if (!isSignedIn) {
     await signin();
     return;
   }
 
-  log.info("user already signed in!");
+  log.info('user already signed in!');
 });
 
 app.event('signin', async ({ send, userGraph }) => {
-  const me = await userGraph.me.get();
+  const me = await userGraph.call(endpoints.me.get);
 
   await send(
     new MessageActivity(`hello ${me.displayName} 👋!`).addCard(
-      "adaptive",
+      'adaptive',
       new AdaptiveCard(
         new CodeBlock({
           codeSnippet: JSON.stringify(me, null, 2),
--- packages/cli/templates/typescript/tab/src/Tab/App.tsx
+++ packages/cli/templates/typescript/tab/src/Tab/App.tsx
@@ -1,33 +1,107 @@
 import React from 'react';
 import * as client from '@microsoft/teams.client';
 import { ConsoleLogger } from '@microsoft/teams.common';
+import * as endpoints from '@microsoft/teams.graph-endpoints';
+import * as teamsJs from '@microsoft/teams-js';
 
 import './App.css';
 
+const clientId = import.meta.env.VITE_CLIENT_ID;
+
 export default function App() {
-  const [greeting, setGreeting] = React.useState('');
+  const [content, setContent] = React.useState('');
+  const [app, setApp] = React.useState<client.App | null>(null);
 
   React.useEffect(() => {
     (async () => {
-      const clientId = import.meta.env.VITE_CLIENT_ID;
-      const tenantId = import.meta.env.VITE_TENANT_ID;
+
+      // initialize the app and prompt for Graph scope consent, if not already granted
       const app = new client.App(clientId, {
-        tenantId,
         logger: new ConsoleLogger('@tests/tab', { level: 'debug' }),
+        msalOptions: {
+          prewarmScopes: ['User.Read', 'Presence.ReadWrite', 'Team.ReadBasic.All']
+        }
       });
 
       await app.start();
-
-      const result = await app.exec<string>('hello-world');
-      setGreeting(result);
+      app.log.info('app started');
+      setApp(app);
     })();
   }, []);
 
+  const showTeamsJsContext = React.useCallback(async () => {
+    if (!app) {
+      return;
+    }
+
+    const context = await teamsJs.app.getContext();
+    setContent(JSON.stringify(context, null, 2));
+  }, [app]);
+
+
+  const postChatMessage = React.useCallback(async () => {
+    if (!app) {
+      return;
+    }
+
+    // get the bot to post a message to the current chat, whichever that is
+    const { conversationId } = await app.exec<{ conversationId: string }>('post-to-chat', { message: 'Hello from the client!' });
+    setContent(`Message posted to conversation ${conversationId}`);
+  }, [app]);
+
+
+  const whoAmI = React.useCallback(async () => {
+    if (!app) {
+      return;
+    }
+
+    // get the current user from the Microsoft Graph
+    const me = await app.graph.call(endpoints.me.get);
+    setContent(JSON.stringify(me, null, 2));
+  }, [app]);
+
+
+  const togglePresentationMode = React.useCallback(async () => {
+    if (!app) {
+      return;
+    }
+
+    // get current presence from the Microsoft Graph
+    const { availability } = await app.graph.call(endpoints.me.presence.get);
+    const isAvailable = availability === 'Available';
+
+    // toggle between Dnd/Presenting and Available/Available
+    const newPresence = {
+      sessionId: clientId,
+      availability: isAvailable ? 'DoNotDisturb' : 'Available',
+      activity: isAvailable ? 'Presenting' : 'Available'
+    };
+    await app.graph.call(endpoints.me.presence.setPresence.create, newPresence);
+    setContent(`You're now ${newPresence.activity}`);
+
+  }, [app]);
+
   return (
     <div className="App">
-      <pre>
-        <code>{greeting}</code>
-      </pre>
+      <h1>👋 Welcome</h1>
+      <p>This test app lets you try out some of the features offered by Teams AI Library v2 for Teams Tab app developers.</p>
+
+      <div className="actions">
+        <button disabled={!app} onClick={showTeamsJsContext}>Show TeamsJs context</button>
+        <button disabled={!app} onClick={postChatMessage}>Post chat message</button>
+        <button disabled={!app} onClick={whoAmI}>Who am I?</button>
+        <button disabled={!app} onClick={togglePresentationMode}>Toggle presentation mode</button>
+      </div>
+
+      {content && (
+        <div className='result'>
+          <pre>
+            <code>{content}</code>
+          </pre>
+        </div>
+      )}
+
+      <p>For more information, please refer to the <a href='https://microsoft.github.io/teams-ai' rel='noopener noreferrer' target='_blank'>Teams AI documentation</a>.</p>
     </div>
   );
 }
