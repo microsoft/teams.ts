3711a314c2596f312233fec87fc7736102a55b16
heyitsaamir <ajawaid191@gmail.com>
2025-10-27 12:40:46 -0700

    Add MessageExtensionAttachmentLayout

---
+++ packages/api/src/models/messaging-extension/message-extension-attachment-layout.ts
@@ -0,0 +1,1 @@
+export type MessageExtensionAttachmentLayout = 'list' | 'grid';
--- packages/api/src/models/messaging-extension/messaging-extension-result.ts
+++ packages/api/src/models/messaging-extension/messaging-extension-result.ts
@@ -1,6 +1,6 @@
 import { Activity } from '../../activities';
-import { AttachmentLayout } from '../attachment';
 
+import { MessageExtensionAttachmentLayout } from './message-extension-attachment-layout';
 import { MessagingExtensionAttachment } from './messaging-extension-attachment';
 import { MessagingExtensionSuggestedAction } from './messaging-extension-suggested-action';
 
@@ -27,10 +27,10 @@ export type MessagingExtensionResultType =
  */
 export type MessagingExtensionResult = {
   /**
-   * @member {AttachmentLayout} [attachmentLayout] Hint for how to deal with
+   * @member {MessageExtensionAttachmentLayout} [attachmentLayout] Hint for how to deal with
    * multiple attachments. Possible values include: 'list', 'grid'
    */
-  attachmentLayout?: AttachmentLayout;
+  attachmentLayout?: MessageExtensionAttachmentLayout;
 
   /**
    * @member {MessagingExtensionResultType} [type] The type of the result. Possible values include:
