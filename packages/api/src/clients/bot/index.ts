bbabd80c94a15b9c1b27587ca18c0dc8e1d6e4dc
heyitsaamir <ajawaid191@gmail.com>
2025-11-11 15:45:22 -0800

    Remove bot token client

--- packages/api/src/clients/bot/index.ts
+++ packages/api/src/clients/bot/index.ts
@@ -3,17 +3,14 @@ import { Client, ClientOptions } from '@microsoft/teams.common/http';
 import { ApiClientSettings, mergeApiClientSettings } from '../api-client-settings';
 
 import { BotSignInClient } from './sign-in';
-import { BotTokenClient } from './token';
 
 export class BotClient {
-  readonly token: BotTokenClient;
   readonly signIn: BotSignInClient;
 
   get http() {
     return this._http;
   }
   set http(v) {
-    this.token.http = v;
     this.signIn.http = v;
     this._http = v;
   }
@@ -30,10 +27,8 @@ export class BotClient {
     }
 
     this._clientSettings = mergeApiClientSettings(clientSettings);
-    this.token = new BotTokenClient(this.http, this._clientSettings);
     this.signIn = new BotSignInClient(this.http, this._clientSettings);
   }
 }
 
 export * from './sign-in';
-export * from './token';
