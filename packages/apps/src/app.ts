fe0ae299da45e97bb46b33bec9f9485f46eb29ab
heyitsaamir <ajawaid191@gmail.com>
2025-11-10 17:17:45 -0800

    remove dep in name

--- packages/apps/src/app.ts
+++ packages/apps/src/app.ts
@@ -129,7 +129,6 @@ export class App<TPlugin extends IPlugin = IPlugin> {
    * @deprecated Name will be removed in the near future. Please remove dependencies from it.
    */
   get name() {
-    this.log.warn('app.name will be removed in the future');
     return this._manifest.name?.full;
   }
 
