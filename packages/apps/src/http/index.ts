38972dedfb8312eb7a2d3d2f84eabd91ff9a00e0
heyitsaamir <ajawaid191@gmail.com>
2026-01-20 10:59:16 -0800

    Fix

--- packages/apps/src/http/index.ts
+++ packages/apps/src/http/index.ts
@@ -1,4 +1,4 @@
-export * from './adapter';
-export * from './express-adapter';
+// Public API exports
+export { IHttpAdapter, IRouteConfig, IRequestHelpers } from './adapter';
+export { ExpressAdapter } from './express-adapter';
 export { IHttpServer } from './http-server';
-export * from './http-stream';
