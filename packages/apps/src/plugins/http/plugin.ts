9557fe638e15eb063a46aaabff3d85fa91949796
Aamir Jawaid <48929123+heyitsaamir@users.noreply.github.com>
2026-03-24 11:21:24 -0700

    Update packages/apps/src/plugins/http/plugin.ts
    Co-authored-by: Corina <14900841+corinagum@users.noreply.github.com>

--- packages/apps/src/plugins/http/plugin.ts
+++ packages/apps/src/plugins/http/plugin.ts
@@ -35,7 +35,7 @@ export class HttpPlugin {
   protected expressAdapter: ExpressAdapter;
   protected server: HttpServer;
 
-  constructor(server?: http.Server, options?: { skipAuth?: boolean; messagingEndpoint?: string }) {
+  constructor(server?: http.Server, options?: { skipAuth?: boolean; messagingEndpoint?: `/${string}` }) {
     this.expressAdapter = new ExpressAdapter(server);
     this.server = new HttpServer(this.expressAdapter, {
       ...options,
