d0cc00677a42e5138c2d3e93ef9e3f3876ae199a
Aamir Jawaid <48929123+heyitsaamir@users.noreply.github.com>
2026-03-23 23:13:32 -0700

    Update index.ts

--- examples/echo/src/index.ts
+++ examples/echo/src/index.ts
@@ -8,7 +8,6 @@ import { MockReminderService } from './mock-reminder-service';
 const app = new App({
   logger: new ConsoleLogger('@tests/echo', { level: 'debug' }),
   plugins: [new DevtoolsPlugin()],
-  messagingEndpoint: '/my-endpoint',
 });
 
 app.on('message', async ({ reply, activity }) => {
